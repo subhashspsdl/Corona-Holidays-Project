3597d097b0d22d88a53797408797573d8deca0bd
spsd <subhash99.d@gmail.com>
2020-03-30 07:30:51 +0530

    Created Spring project with spring initializer

---
+++ src/main/java/com/example/corona_holidays_projects/CoronaHolidaysProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.corona_holidays_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaHolidaysProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaHolidaysProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corona_holidays_projects/CoronaHolidaysProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corona_holidays_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaHolidaysProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
